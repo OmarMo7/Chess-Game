9b9988745bfe53934d1ccdb241584f902bfdbbdd
OmarMo7 <omar7.first@gmail.com>
2021-03-20 02:31:12 +0200

    Initiation - setting position

--- src/chess_game/main.java
+++ src/chess_game/main.java
@@ -3,19 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package chess_game;
 /**
  *
  * @author Omar Mostafa
  */
-public class Piece {
+public class main {
 
-    private static class position {
-        int x_axis, y_axis;
-
-        public position(int x_axis, int y_axis) {
-            this.x_axis = x_axis;
-            this.y_axis = y_axis;
-        }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
     }
+    
 }
