e90581dae04476cbd59576dfaf6859ae392705bc
OmarMo7 <omar7.first@gmail.com>
2021-03-20 02:42:16 +0200

    Constructing the class

---
+++ src/chess_game/Game.java
@@ -0,0 +1,25 @@
+package chess_game;
+
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Omar Mostafa
+ */
+public class Game {
+    public  ArrayList <Piece> piecesOfPlayer1 = new ArrayList();
+    public static ArrayList <Piece> piecesOfPlayer2 = new ArrayList();
+    
+    public Game(){
+        
+    }
+    Game g = new Game();
+    
+}
