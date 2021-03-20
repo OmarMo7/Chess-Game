ce01322985282d1736f48f7c56d47e6979200014
OmarMo7 <omar7.first@gmail.com>
2021-03-21 00:47:51 +0200

    Pawn's basic moves established

--- src/chess_game/Piece.java
+++ src/chess_game/Piece.java
@@ -1,7 +1,6 @@
 package chess_game;
 
-
-
+import static chess_game.Board.board;
 import java.util.ArrayList;
 
 /*
@@ -21,8 +20,8 @@ public class Piece {
     int x_axis, y_axis;
     boolean isThreatened = false;
     boolean isEaten = false;
-    
-    public Piece(String material_name, String material_color, int x_axis, int y_axis){
+
+    public Piece(String material_name, String material_color, int x_axis, int y_axis) {
         this.name = material_name;
         this.color = material_color;
         this.x_axis = x_axis;
@@ -33,18 +32,45 @@ public class Piece {
 
     ArrayList calculateCells() {
         switch (this.name) {
-            case "Pawn":
-            //do something
-                
-            case "Knight":
+        case "Pawn":
+            if (this.y_axis >= 7) // Precation condition
+                break;
+            if (this.y_axis < 6) {
+                // Classic move of a pawn
+                if (!board[this.x_axis][this.y_axis + 1].isOccupied
+                        && !board[this.x_axis][this.y_axis + 2].isOccupied) {
+                    cellsAllowed.add(board[this.x_axis][this.y_axis + 1]);
+                    cellsAllowed.add(board[this.x_axis][this.y_axis + 2]);
+                } else if (!board[this.x_axis][this.y_axis + 1].isOccupied) {
+                    cellsAllowed.add(board[this.x_axis][this.y_axis + 1]);
+                }
+
+            } else {
+                if (!board[this.x_axis][this.y_axis + 1].isOccupied) {
+                    cellsAllowed.add(board[this.x_axis][this.y_axis + 1]);
+                }
+            }
+
+            // Checking if the cells at right-left of a pawn is occupied - Eat move
+            for (int i = 3; i > 0; i--) {
+                int b = i - 2;
+                if (b == 2) // The Cell ahead is Already checked above
+                    continue;
+                // If there is a material at either left or right up ahead you can go either
+                if (board[this.x_axis - b][this.y_axis + 1].isOccupied) {
+                    cellsAllowed.add(board[this.x_axis - b][this.y_axis + 1]);
+                }
+            }
+
+        case "Knight":
             //
-            case "Bishop":
+        case "Bishop":
             //
-            case "Rook":
+        case "Rook":
             //
-            case "Queen":
+        case "Queen":
             //
-            case "King":
+        case "King":
             //
         }
         return cellsAllowed;
