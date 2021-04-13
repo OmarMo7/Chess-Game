5868c4a1982674c10a1419a8f7074eb0c73130b7
OmarMo7 <omar7.first@gmail.com>
2021-04-14 00:20:55 +0200

    Aliging the board cell names properly

--- src/chess_game/Game.java
+++ src/chess_game/Game.java
@@ -1,7 +1,7 @@
 package chess_game;
 
-
-import java.util.ArrayList;
+import java.util.Scanner;
+import static chess_game.Board.board;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,12 +14,107 @@ import java.util.ArrayList;
  * @author Omar Mostafa
  */
 public class Game {
-    public  ArrayList <Piece> piecesOfPlayer1 = new ArrayList();
-    public static ArrayList <Piece> piecesOfPlayer2 = new ArrayList();
-    
-    public Game(){
-        
+    // public ArrayList <Piece> piecesOfPlayer1 = new ArrayList();
+    // public static ArrayList <Piece> piecesOfPlayer2 = new ArrayList();
+    public static String cellLettersNumbers[] = new String[] { "a", "b", "c", "d", "e", "f", "g", "h", "1", "2", "3",
+            "4", "5", "6", "7", "8" };
+
+    public Game() {
+
     }
-    Game g = new Game();
-    
+
+    public void start() {
+        Board b = new Board();
+        b.construct(b);
+        displayBoard();
+        // TODO: display options
+    }
+
+    // TODO: optimization to be performed - to check both first and second
+    // letter together instead of checking each one at once
+
+    public void move(Piece pieceToBeMoved, int x_axis, int y_axis) {
+        String x = new String();
+        String y = new String();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the cell name as x * y");
+        boolean existX = false;
+        do {
+            System.out.println("Enter x");
+            x = input.next();
+            for (int i = 0; i < cellLettersNumbers.length; i++) {
+                if (x.length() > 1)// Another message to be displayed here
+                    break;
+                if (cellLettersNumbers[i] == x) {
+                    existX = true;
+                    break;
+                } else if (i == cellLettersNumbers.length - 1) {
+                    existX = false;
+                    System.out.println("Not in-board cell");
+                }
+            }
+        } while (!existX);
+        boolean existY = false;
+        do {
+            System.out.println("Enter y");
+            y = input.next();
+            for (int i = 0; i < cellLettersNumbers.length; i++) {
+                if (y.length() > 1)// Another message to be displayed here
+                    break;
+                if (cellLettersNumbers[i] == y) {
+                    existY = true;
+                    break;
+                } else if (i == cellLettersNumbers.length - 1) {
+                    existY = false;
+                    System.out.println("Not in-board cell");
+                }
+            }
+        } while (!existY);
+        input.close();
+        if (pieceToBeMoved.cellsAllowed.contains(board[Integer.parseInt(x)][Integer.parseInt(y)])) {
+            pieceToBeMoved.setPosition(Integer.parseInt(x), Integer.parseInt(y));
+        } else {
+            System.out.println("Not available cell for such a piece!");
+        }
+    }
+
+    public void displayBoard() {
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[j][i].isOccupied) {
+                    if (board[j][i].takenBy.name == "Pawn") {
+                        if (j == 7) {
+                            System.out.print("   " + "Pawn" + " ");
+                        } else
+                            System.out.print("  " + "Pawn" + " ");
+                    } else
+                        System.out.print(" " + board[j][i].takenBy.name + " ");
+                } else {
+                    System.out.print("------|");
+                }
+                if (j == 7)
+                    System.out.print("\n");
+            }
+        }
+        // Black perspective
+        // for (int i = 0; i < 8; i++) {
+        // for (int j = 0; j < 8; j++) {
+        // if (board[j][i].isOccupied) {
+        // if (board[j][i].takenBy.name == "Pawn") {
+        // if (j == 3 || j == 7) {
+        // System.out.print(board[j][i].name + " " + board[j][i].takenBy.name);
+        // } else
+        // System.out.print(board[j][i].name + " " + board[j][i].takenBy.name);
+        // } else
+        // System.out.print(board[j][i].name + " " + board[j][i].takenBy.name);
+        // } else {
+        // System.out.print(board[j][i].name + " ");
+        // }
+        // if (j == 7)
+        // System.out.print("\n");
+        // }
+        // }
+    }
+
 }
